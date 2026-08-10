b2d1166f5f8970312a024f39ab51db85c14fcc5f
Peter Pfeufer <info@ppfeufer.de>
2026-08-10 09:51:37 +0200

    [CHANGE] Restructured some things
    ### Changed
    
    - Restructured some things

--- .make/conf.d/development.mk
+++ .make/conf.d/development.mk
@@ -1,21 +1,23 @@
-.PHONY: lint
-lint:
-	@echo "Linting JavaScript files …"
-	@npm run lint
+.PHONY: lint-css
+lint-css:
+	@echo "Linting CSS files …"
+	@npm run lint:css
 
-.PHONY: minify
-minify:
-	@echo "Minifying JavaScript files …"
-	@npm run minify
+.PHONY: lint-js
+lint-js:
+	@echo "Linting JavaScript files …"
+	@npm run lint:js
+
+.PHONE: lint
+lint: lint-css lint-js
 
 .PHONY: build
 build:
-	@echo "Building JavaScript files …"
+	@echo "Building distribution files …"
 	@npm run build
 
 help::
 	@echo "  $(TEXT_UNDERLINE)Development:$(TEXT_UNDERLINE_END)"
-	@echo "    build                     Build JavaScript files"
+	@echo "    build                     Build distribution files"
 	@echo "    lint                      Lint JavaScript files"
-	@echo "    minify                    Minify JavaScript files"
 	@echo ""
