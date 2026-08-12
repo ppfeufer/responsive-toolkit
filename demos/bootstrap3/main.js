142c1cca2dc2a7c01acdf25d2b417ac0b4ea7766
Peter Pfeufer <info@ppfeufer.de>
2026-08-12 11:26:39 +0200

    [FIX] JS and JS examples in demo pages
    ### Fixed
    
    - JS and JS examples in demo pages

--- demos/bootstrap3/main.js
+++ demos/bootstrap3/main.js
@@ -1,35 +1,36 @@
 /* global ResponsiveToolkit */
 
-(($, document, window, viewport) => {
+(($, viewport) => {
     'use strict';
 
+    // Use Bootstrap 3 (This needs to be done before the document ready event)
     viewport.use('Bootstrap3');
 
-    const updateDemo = () => {
-        const currentBreakpoint = viewport.current();
-
-        $('.breakpoint-alias').text(currentBreakpoint.toUpperCase());
-        $('.comparison-operator').each((unusedIndex, element) => {
-            void unusedIndex;
-            const $element = $(element);
-
-            $element.toggleClass('active', viewport.is($element.data('expression')));
-        });
-    };
-
     $(document).ready(() => {
+        const updateDemo = () => {
+            const currentBreakpoint = viewport.current();
+
+            $('.breakpoint-alias').text(currentBreakpoint.toUpperCase());
+            $('.comparison-operator').each((unusedIndex, element) => {
+                void unusedIndex;
+                const $element = $(element);
+
+                $element.toggleClass('active', viewport.is($element.data('expression')));
+            });
+        };
+
         updateDemo();
         console.log('Current breakpoint:', viewport.current());
+
+        $(window).resize(
+            viewport.changed(() => {
+                updateDemo();
+                console.log('Current breakpoint:', viewport.current());
+            })
+        );
+
+        if (window.hljs && typeof window.hljs.highlightAll === 'function') {
+            window.hljs.highlightAll();
+        }
     });
-
-    $(window).resize(
-        viewport.changed(() => {
-            updateDemo();
-            console.log('Current breakpoint:', viewport.current());
-        })
-    );
-})(jQuery, document, window, ResponsiveToolkit);
-
-if (window.hljs && typeof window.hljs.highlightAll === 'function') {
-    window.hljs.highlightAll();
-}
+})(jQuery, ResponsiveToolkit);
--- demos/default/main.js
+++ demos/default/main.js
@@ -1,36 +1,36 @@
 /* global ResponsiveToolkit */
 
-(($, document, window, viewport) => {
+(($, viewport) => {
     'use strict';
 
-    // Intentionally do not call viewport.use(...): this demo shows default toolkit behavior.
-    const updateDemo = () => {
-        const currentBreakpoint = viewport.current();
-
-        $('.breakpoint-alias').text(currentBreakpoint.toUpperCase());
-        $('.comparison-operator').each((unusedIndex, element) => {
-            void unusedIndex;
-            const $element = $(element);
-
-            $element.toggleClass('active', viewport.is($element.data('expression')));
-        });
-    };
-
     $(document).ready(() => {
+        // Intentionally do not call viewport.use(...): this demo shows default toolkit behavior.
+        const updateDemo = () => {
+            const currentBreakpoint = viewport.current();
+
+            $('.breakpoint-alias').text(currentBreakpoint.toUpperCase());
+            $('.comparison-operator').each((unusedIndex, element) => {
+                void unusedIndex;
+                const $element = $(element);
+
+                $element.toggleClass('active', viewport.is($element.data('expression')));
+            });
+        };
+
         updateDemo();
 
         console.log('Current breakpoint:', viewport.current());
+
+        $(window).resize(
+            viewport.changed(() => {
+                updateDemo();
+
+                console.log('Current breakpoint:', viewport.current());
+            })
+        );
+
+        if (window.hljs && typeof window.hljs.highlightAll === 'function') {
+            window.hljs.highlightAll();
+        }
     });
-
-    $(window).resize(
-        viewport.changed(() => {
-            updateDemo();
-
-            console.log('Current breakpoint:', viewport.current());
-        })
-    );
-})(jQuery, document, window, ResponsiveToolkit);
-
-if (window.hljs && typeof window.hljs.highlightAll === 'function') {
-    window.hljs.highlightAll();
-}
+})(jQuery, ResponsiveToolkit);
